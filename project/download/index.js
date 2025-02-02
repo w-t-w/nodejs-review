9c2c185cc486726557f26d5b5344d73bfcb08364
w-t-w <15866369958@163.com>
2025-02-02 10:26:34 +0800

    feature: NodeJS 极客时间 download 下载部分网页项目练习部分回溯已顺利完成

---
+++ project/download/index.js
@@ -0,0 +1,26 @@
+// const path = require('path');
+// const fs = require('fs');
+// const Koa = require('koa');
+// const KoaMount = require('koa-mount');
+// const KoaStatic = require('koa-static');
+//
+// const PORT = 3000;
+//
+// const TEMPLATE_DIR = path.resolve(__dirname, './template/index.html');
+// const STATIC_DIR = path.resolve(__dirname, './source');
+//
+// const koa = new Koa();
+//
+// koa.use(KoaStatic(STATIC_DIR));
+//
+// koa.use(KoaMount('/', ctx => {
+//     const {response} = ctx;
+//     response.status = 200;
+//     response.body = fs.readFileSync(TEMPLATE_DIR, 'utf-8');
+// }));
+//
+// koa.listen(PORT, () => {
+//     console.log(`The server is running at http://localhost:${PORT}!`);
+// });
+
+console.log('NodeJS《第十八课: 极客时间 download 下载部分网页项目练习》已回顾完毕并通过!如需查看使用,将上述代码的注释剃掉删除即可!');
---
+++ project/index.js
@@ -0,0 +1,2 @@
+// 极客时间 download 下载部分网页项目练习
+require('./download');
