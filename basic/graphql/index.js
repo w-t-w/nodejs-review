61ca79ec81c3c52b8c6d82ed2007fd84e77d6c43
w-t-w <15866369958@163.com>
2025-02-04 16:39:29 +0800

    feature: NodeJS GraphQL API 接口规范化的查询方式(语言)部分回溯已顺利完成

---
+++ basic/graphql/index.js
@@ -0,0 +1,22 @@
+// const {graphql, buildSchema} = require('graphql');
+//
+// const schema = buildSchema(`
+//     type Query {
+//         hello: String!
+//     }
+// `);
+//
+// const rootValue = {
+//     hello: () => {
+//         return 'Hello GeekBang!!!!!!';
+//     }
+// };
+//
+// graphql({schema, source: '{ hello }', rootValue}).then(({errors, data}) => {
+//     if (errors && errors.length) return console.error(errors);
+//     console.log(data, data.hello);
+// });
+
+console.log('NodeJS《第二十一课: NodeJS GraphQL API 接口规范化的查询方式(语言)》已回顾完毕并通过!如需查看使用,将上述代码的注释剃掉删除即可!');
+
+
