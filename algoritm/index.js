751d651d5ae850cb7a2e28dd8ff85ee946103894
w-t-w <15866369958@163.com>
2025-02-10 21:10:43 +0800

    NodeJS algoritm 抽奖抽卡算法部分回溯已顺利完成

---
+++ algoritm/index.js
@@ -0,0 +1,33 @@
+// const lessonIds = [
+//     "136797",
+//     "136798",
+//     "136799",
+//     "136800",
+//     "136801",
+//     "136803",
+//     "136804",
+//     "136806",
+//     "136807",
+//     "136808",
+//     "136809",
+//     "141994",
+//     "143517",
+//     "143557",
+//     "143564",
+//     "143644",
+//     "146470",
+//     "146569",
+//     "146582"
+// ];
+// let lessonIds_length = lessonIds.length;
+//
+// for (let i = 0; i < 10; i++) {
+//     lessonIds_length = lessonIds.length;
+//     const random = Math.floor(Math.random() * lessonIds_length);
+//     console.log(lessonIds[random]);
+//     lessonIds[random] = lessonIds[lessonIds_length - 1];
+//     lessonIds.length--;
+//     // lessonIds.splice(random, 1);
+// }
+
+console.log('NodeJS《第三十一课: algoritm 抽奖抽卡算法》已回顾完毕并通过!如需查看使用,将上述代码的注释剃掉删除即可!');
